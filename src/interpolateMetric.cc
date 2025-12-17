c4e424ee7b71713b41a03375ad09082f34ca8d7d
njf4697 <njf4697@rit.edu>
2025-12-17 11:06:42 -0500

    enforce grid bounds and tweak interpolation to always match CarpetX

--- src/interpolateMetric.cc
+++ src/interpolateMetric.cc
@@ -1,4 +1,5 @@
 #include "raytracingx.h"
+#include <string.h>
 
 #define NUM_INTERP_POINTS 1
 #define NUM_GRID_ARRAYS 10
@@ -43,6 +44,13 @@ void interpolateMetricAtPoint(CCTK_ARGUMENTS, const CCTK_REAL x, const CCTK_REAL
     interp_x[0] = x;
     interp_y[0] = y;
     interp_z[0] = z;
+
+    assert(x > xmin);
+    assert(x < xmax);
+    assert(y > ymin);
+    assert(y < ymax);
+    assert(z > zmin);
+    assert(z < zmax);
 
     const void* interp_coords[3];
     interp_coords[0] = (const CCTK_REAL*) interp_x;
@@ -114,10 +122,12 @@ void interpolateMetricAtPoint(CCTK_ARGUMENTS, const CCTK_REAL x, const CCTK_REAL
       operation_codes[i] = 0;
     }
 
-    int param_table_handle = Util_TableCreateFromString("order=4");
+    std::string orderstr = "order=" + std::to_string(interpolation_order); //from CarpetX
+
+    int param_table_handle = Util_TableCreateFromString(orderstr);
     if (param_table_handle < 0) {
     CCTK_VWarn(0, __LINE__, __FILE__, CCTK_THORNSTRING,
-               "bad interpolator parameter(s) \"%s\"!", "order=3");
+               "bad interpolator parameter(s) \"%s\"!", orderstr);
     }
 
     Util_TableSetIntArray(param_table_handle, NUM_GRID_ARRAYS,
