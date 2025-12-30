503e578734ed8e2147186e4ac823995512c464c6
njf4697 <njf4697@rit.edu>
2025-12-29 21:46:22 -0600

    test new interp method

--- src/interpolateMetric.cc
+++ src/interpolateMetric.cc
@@ -110,18 +110,20 @@ void interpolateMetricAtPoint(CCTK_ARGUMENTS, const CCTK_REAL x, const CCTK_REAL
 
     metric_at_point.metric0pr = sqrt(pow(metric_at_point.metric[0],2) - metric_at_point.metric[1]*metric_at_point.beta_up[0] - metric_at_point.metric[2]*metric_at_point.beta_up[1] - metric_at_point.metric[3]*metric_at_point.beta_up[2]); //g_{00}
 
-    CCTK_WARN(1, ("camera position: x: " + std::to_string(x) + ", y: " + std::to_string(y) + ", z: " + std::to_string(z)).c_str());
-    CCTK_WARN(1, ("alpha: " + std::to_string(metric_at_point.metric[0])).c_str());
-    CCTK_WARN(1, ("beta: (" + std::to_string(metric_at_point.beta_up[0]) + ", " + std::to_string(metric_at_point.beta_up[1]) + ", " + std::to_string(metric_at_point.beta_up[2]) + ")").c_str());
-    CCTK_WARN(1, ("metric1: (" + std::to_string(metric_at_point.metric0pr) + ", " + std::to_string(metric_at_point.metric[1]) + ", " + std::to_string(metric_at_point.metric[2]) + ", " + std::to_string(metric_at_point.metric[3]) + ")").c_str());
-    CCTK_WARN(1, ("metric2: (" + std::to_string(metric_at_point.metric[1]) + ", " + std::to_string(metric_at_point.metric[4]) + ", " + std::to_string(metric_at_point.metric[5]) + ", " + std::to_string(metric_at_point.metric[6]) + ")").c_str());
-    CCTK_WARN(1, ("metric3: (" + std::to_string(metric_at_point.metric[2]) + ", " + std::to_string(metric_at_point.metric[5]) + ", " + std::to_string(metric_at_point.metric[7]) + ", " + std::to_string(metric_at_point.metric[8]) + ")").c_str());
-    CCTK_WARN(1, ("metric4: (" + std::to_string(metric_at_point.metric[3]) + ", " + std::to_string(metric_at_point.metric[6]) + ", " + std::to_string(metric_at_point.metric[8]) + ", " + std::to_string(metric_at_point.metric[9]) + ")").c_str());
+    printf(("camera position: x: " + std::to_string(x) + ", y: " + std::to_string(y) + ", z: " + std::to_string(z)).c_str());
+    printf(("alpha: " + std::to_string(metric_at_point.metric[0])).c_str());
+    printf(("beta: (" + std::to_string(metric_at_point.beta_up[0]) + ", " + std::to_string(metric_at_point.beta_up[1]) + ", " + std::to_string(metric_at_point.beta_up[2]) + ")").c_str());
+    printf(("metric1: (" + std::to_string(metric_at_point.metric0pr) + ", " + std::to_string(metric_at_point.metric[1]) + ", " + std::to_string(metric_at_point.metric[2]) + ", " + std::to_string(metric_at_point.metric[3]) + ")").c_str());
+    printf(("metric2: (" + std::to_string(metric_at_point.metric[1]) + ", " + std::to_string(metric_at_point.metric[4]) + ", " + std::to_string(metric_at_point.metric[5]) + ", " + std::to_string(metric_at_point.metric[6]) + ")").c_str());
+    printf(("metric3: (" + std::to_string(metric_at_point.metric[2]) + ", " + std::to_string(metric_at_point.metric[5]) + ", " + std::to_string(metric_at_point.metric[7]) + ", " + std::to_string(metric_at_point.metric[8]) + ")").c_str());
+    printf(("metric4: (" + std::to_string(metric_at_point.metric[3]) + ", " + std::to_string(metric_at_point.metric[6]) + ", " + std::to_string(metric_at_point.metric[8]) + ", " + std::to_string(metric_at_point.metric[9]) + ")").c_str());
 
     // Destroy the parameter table
     Util_TableDestroy(paramTableHandle);
 
     calculateInverseMetric(metric_at_point); //get g^{\mu\nu}
+
+    CCTK_VERROR("test completed");
 } 
 
 /* old version
