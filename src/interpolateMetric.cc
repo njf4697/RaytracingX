c5168301facd9420b1995cf6e0bc50b9296fff78
njf4697 <njf4697@rit.edu>
2026-01-01 21:42:41 -0500

    ensure only one processor runs

--- src/interpolateMetric.cc
+++ src/interpolateMetric.cc
@@ -65,7 +65,7 @@ void interpolateMetricAtPoint(CCTK_ARGUMENTS, const CCTK_REAL x, const CCTK_REAL
 
     // Perform the interpolation
     ierr = DriverInterpolate(cctkGH, 3, interpHandle, paramTableHandle,
-                             coordSystemHandle, NUM_INTERP_POINTS, interpCoordsTypeCode,
+                             coordSystemHandle, nPoints, interpCoordsTypeCode,
                              interpCoords, nInputArrays, inputArrayIndices,
                              nInputArrays, outputArrayTypes, outputArrays);
 
