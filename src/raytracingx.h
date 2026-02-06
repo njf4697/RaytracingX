660bd22ba6f885d25f95f482c8c4c2cd99e8715f
njf4697 <njf4697@rit.edu>
2026-02-05 20:00:45 -0500

    testing integration

--- src/raytracingx.h
+++ src/raytracingx.h
@@ -64,7 +64,7 @@ void vectorToOneForm(CCTK_REAL* X_oneform, const CCTK_REAL* X_vector, const Metr
 void vectorToOneFormArr(CCTK_REAL* X_oneform, const CCTK_REAL* X_vector, const CCTK_REAL* arr); //utilities.cc
 void projectUontoV(CCTK_REAL* X, const CCTK_REAL* U, const CCTK_REAL* V, const Metric* m); //utilities.cc
 void normalize(CCTK_REAL* X_norm, const CCTK_REAL* X, const Metric* m); //utilities.cc
-CCTK_REAL getTimeComponentOf4Velocity(const CCTK_REAL vx, const CCTK_REAL vy, const CCTK_REAL vz, const Metric* m); //utilities.cc
+CCTK_REAL getTimeComponentOf4Velocity(const CCTK_REAL vx, const CCTK_REAL vy, const CCTK_REAL vz, Metric* m); //utilities.cc
 template <typename StructType, typename ParticleContainerClass>
 void camera_initializer(ParticleContainerClass &pc, const CCTK_REAL *real_params, const CCTK_INT *int_params);
 void setup_camera_initializer_ints(CCTK_ARGUMENTS, CCTK_INT* int_params);
