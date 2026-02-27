d5fbfdb9cc935d3bbf8909ebae799f4cb1ab20a1
njf4697 <njf4697@rit.edu>
2026-02-27 18:27:21 -0500

    change debug statements, fix particle generation

--- src/photons.cc
+++ src/photons.cc
@@ -222,6 +222,7 @@ void camera_initializer(ParticleContainerClass &pc, const CCTK_REAL *real_params
 
         #pragma omp parallel for
         for (int local_particle_id = 0; local_particle_id < particles_per_tile; ++local_particle_id) {//create 4-vector \chi parallel to geodesic and fill geodesic initial conditions for each pixel (see https://arxiv.org/pdf/1410.777)
+                int pidx = local_offset + local_particle_id;
 
                 int i = pidx / num_pixels_width;
                 int j = pidx % num_pixels_width;
